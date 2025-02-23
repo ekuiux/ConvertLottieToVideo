4d525aeff89480904a692636564f0a54d616546b
ekuiux <ekdesweb@gmail.com>
2025-02-23 19:18:38 +0200

    Update axiosInstance.js

--- client-react/src/api/axiosInstance.js
+++ client-react/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5001/convert", // Замените на ваш URL
+  baseURL: "https://cltv-server.onrender.com", // Замените на ваш URL
   timeout: 5000, // Таймаут запроса (по желанию)
   headers: {
     "Content-Type": "application/json",
